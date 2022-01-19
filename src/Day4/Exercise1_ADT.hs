7fa7e351d81b775ae933cc6c4fc52431df3c80f9
Oleg Romashin <oleg.romashin@metalamp.io>
2022-01-19 18:49:45 +0700

    extended exercise on ADT

--- src/Day4/Exercise1_ADT.hs
+++ src/Day4/Exercise1_ADT.hs
@@ -1,7 +1,10 @@
 module Day4.Exercise1_ADT where
 
 {- Create an ADT for different entities:
-   Customer should contain the following fields: id, first name, last name.
+   Customer should contain the following fields: id, first name, optional middle name, last name.
    Credit should contain the following fields: id, borrower id, loan amount, monthly payment.
    Card type with two possible values: Debit and Credit.
    Card should contain the following fields: card number, card holder id, card type, security code. -}
+
+{- Derive "Show" instances for "Customer", "Credit" and "Card type" types.
+   Write custom "Show" instance for "Card" type without showing security code. -}
