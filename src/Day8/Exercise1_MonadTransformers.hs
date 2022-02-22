2b6f5b08cbefa3a1c4eca83d8594ae2508a004a5
Odeyaloleg <Odeyaloleg@gmail.com>
2022-02-22 19:45:26 +0700

    added exercise on monad transformers

---
+++ src/Day8/Exercise1_MonadTransformers.hs
@@ -0,0 +1,5 @@
+module Day8.Exercise1_MonadTransformers where
+
+{- Add configurator library to dependencies, create and parse config with 3 custom settings.
+   Run computration in ReaderT monad from IO with config as environment.
+   Print all settings from config inside this ReaderT computation. -}
